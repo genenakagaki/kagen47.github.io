747cd045962457cd3243ca552aa8f51b2352294a
Gene Nakagaki <gene.nakagaki@gmail.com>
2018-12-25 23:11:55 -0500

    Added redac project

--- js/projects.js
+++ js/projects.js
@@ -1,79 +1,115 @@
 var projects = [{
-        "title": "sumitomo-ny.com",
-        "subtitle": "Real Estate Web App - October 2018",
-        "imgs": [
-            "sumitomo-1.jpg",
-            "sumitomo-2.jpg",
-            "sumitomo-3.jpg",
-            "sumitomo-4.jpg",
-            "sumitomo-5.jpg"
-        ],
-        "technologies": [
-            "ASP.NET MVC 5",
-            "Entity Framework",
-            "SQL Server",
-            "Javascript, HTML5, CSS3",
-            "jQuery",
-            "Bootstrap",
-            "Google Maps Javascript API"
-        ],
-        "buttons": [
-            "<a class='btn btn-outline-primary' href='https://www.sumitomo-ny.com' target='_blank'><i class='fas fa-external-link-alt'></i> VIEW SITE</a>"
-        ],
-        "description": "A multilingual real estate web application built using the ASP.NET MVC5 framework. This was created and managed by a small team using Git as the version control system.",
-        "details": [
-            "Designed the SQL Server Database to store data for Listings and Agents.",
-            "Created a C# Library that imports data from Multiple Listing Service (MLS), maps it to the database entity object, and then stores it to the database using the Entity Framework.",
-            "Created responsive web app using ASP.NET MVC 5 for the backend, Bootstrap and jQuery for the front-end.",
-            "Created dynamic pages that gets data from the database asynchronously using jQuery.",
-            "Added support for other languages using cookies"
-        ]
-    },
-    {
-        "title": "CMS for sumitomo-ny.com",
-        "subtitle": "CMS Web App - October 2018",
-        "imgs": [
-            "sumitomo-cms-1.jpg",
-            "sumitomo-cms-2.jpg",
-            "sumitomo-cms-3.jpg"
-        ],
-        "technologies": [
-            "ASP.NET MVC 5",
-            "Entity Framework",
-            "SQL Server",
-            "Javascript, HTML5, CSS3",
-            "jQuery",
-            "Bootstrap"
-        ],
-        "buttons": [],
-        "description": "A CMS of news articles for the Sumitomo NY Real Estate Sales web app. The news article added to the CMS is shown on the Real Estate web app.",
-        "details": [
-            "Designed the SQL Server Database to store data for news articles.",
-            "Created a web app that can read and write from the database using ASP.NET MVC 5 for the backend, Bootstrap and jQuery for the front-end.",
-            "Used a content editor Javascript library to allow basic text decoration and positioning, and also adding images."
-        ]
-    },
-    {
-        "title": "care.onkyousa.com",
-        "subtitle": "Web App - March 2018",
-        "imgs": [
-            "onkyo-1.jpg",
-            "onkyo-2.jpg",
-            "onkyo-3.jpg"
-        ],
-        "technologies": [
-            "PHP",
-            "ColdFusion",
-            "Javascript, HTML5, CSS3",
-            "jQuery",
-            "Bootstrap"
-        ],
-        "buttons": [],
-        "description": "A web app for Onkyo customers who are experiencing loss of audio / network connections. The web app was broken and handed over to me. I debugged the code and fixed it.",
-        "details": [
-            "Debugged the web app and found the reason of why the web app was broken.",
-            "Added new feature to insert images and links of tradable models.",
-            "Wrote test code that outputs database design and entries since I did not have access to the database"
-        ]
-    }
+    "title": "sumitomo-ny.com",
+    "subtitle": "Real Estate Web App - October 2018",
+    "imgs": [
+        "sumitomo-1.jpg",
+        "sumitomo-2.jpg",
+        "sumitomo-3.jpg",
+        "sumitomo-4.jpg",
+        "sumitomo-5.jpg"
+    ],
+    "technologies": [
+        "ASP.NET MVC 5",
+        "Entity Framework",
+        "SQL Server",
+        "Javascript, HTML5, CSS3",
+        "jQuery",
+        "Bootstrap",
+        "Google Maps Javascript API",
+        "Git"
+    ],
+    "buttons": [
+        "<a class='btn btn-outline-primary' href='https://www.sumitomo-ny.com' target='_blank'><i class='fas fa-external-link-alt'></i> VIEW SITE</a>"
+    ],
+    "description": "A multilingual real estate web application which users can search for the listings that they want using the filters and the custom map. There are also a lot of resources for the users like area guides, a guide for buying a property, and the guide for different styles of properties.",
+    "details": [
+        "Designed the SQL Server Database to store data for Listings and Agents.",
+        "Created a C# Library that imports data from Multiple Listing Service (MLS), maps it to the database entity object, and then stores it to the database using the Entity Framework.",
+        "Created responsive web app using ASP.NET MVC 5 for the backend, Bootstrap and jQuery for the front-end.",
+        "Created dynamic pages that gets data from the database asynchronously using jQuery.",
+        "Created custom map using Google Maps Javascript API",
+        "Added support for other languages using cookies"
+    ]
+},
+{
+    "title": "CMS for sumitomo-ny.com",
+    "subtitle": "CMS Web App - October 2018",
+    "imgs": [
+        "sumitomo-cms-1.jpg",
+        "sumitomo-cms-2.jpg",
+        "sumitomo-cms-3.jpg"
+    ],
+    "technologies": [
+        "ASP.NET MVC 5",
+        "Entity Framework",
+        "SQL Server",
+        "Javascript, HTML5, CSS3",
+        "jQuery",
+        "Bootstrap",
+        "Git"
+    ],
+    "buttons": [
+        "<a class='btn btn-outline-primary' href='https://www.sumitomo-ny.com' target='_blank'><i class='fas fa-external-link-alt'></i> VIEW SITE</a>"
+    ],
+    "description": "A CMS of news articles for the Sumitomo NY Real Estate Sales web app. The news article added to the CMS is shown on the Real Estate web app.",
+    "details": [
+        "Designed the SQL Server Database to store data for news articles.",
+        "Created a web app that can read and write from the database using ASP.NET MVC 5 for the backend, Bootstrap and jQuery for the front-end.",
+        "Used a content editor Javascript library to allow basic text decoration and positioning, and also adding images."
+    ]
+},
+{
+    "title": "care.onkyousa.com",
+    "subtitle": "Customer Support Web App - March 2018",
+    "imgs": [
+        "onkyo-1.jpg",
+        "onkyo-2.jpg",
+        "onkyo-3.jpg"
+    ],
+    "technologies": [
+        "PHP",
+        "ColdFusion",
+        "Javascript, HTML5, CSS3",
+        "jQuery",
+        "Bootstrap",
+        "Git"
+    ],
+    "buttons": [
+        "<a class='btn btn-outline-primary' href='https://care.onkyousa.com/na/' target='_blank'><i class='fas fa-external-link-alt'></i> VIEW SITE</a>"
+    ],
+    "description": "A web app for Onkyo customers who are experiencing loss of audio / network connections to trade with other models, or return for a discount.",
+    "details": [
+        "Debugged the web app and found the reason of why the web app was broken.",
+        "Wrote test code that outputs database design and entries since I did not have access to the database",
+        "Added new feature to insert images and links of tradable models.",
+        "Maintain the website by adjusting applicable models and updates on the text and UI."
+    ]
+},
+{
+    "title": "redacinc.com",
+    "subtitle": "Real Estate Web App - February 2018",
+    "imgs": [
+        "redac-1.jpg",
+        "redac-2.jpg",
+        "redac-3.jpg"
+    ],
+    "technologies": [
+        "ASP.NET Web Forms",
+        "SQL Server",
+        "Entity Framework",
+        "Javascript, HTML5, CSS3",
+        "jQuery",
+        "Bootstrap",
+        "Git"
+    ],
+    "buttons": [
+        "<a class='btn btn-outline-primary' href='https://www.redacinc.com' target='_blank'><i class='fas fa-external-link-alt'></i> VIEW SITE</a>"
+    ],
+    "description": "A real estate web application which users can search for the listings that they want using the filters and the custom map.",
+    "details": [
+        "Maintained the web app by making small UI updates.",
+        "Updated the top page UI design and made it responsive.",
+        "Added California MLS listings."
+    ]
+}
 ]
